3fe7e456d4aee46dc854259c85926101c54bc95b
rrsoares82 <rodolforodrigues.soares@gmail.com>
2024-09-03 15:20:44 -0300

    version 1.0.1

--- scripts/pos_install.sh
+++ scripts/pos_install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "pos installation checking ..."
+systemctl restart simpleflaskcloudaws
 if [ -f /opt/SimpleFlaskCloudAWS ]; then echo "SimpleFlaskCloudAWS installed on /opt/SimpleFlaskCloudAWS"; else echo "SimpleFlaskCloudAWS NOT installed (/opt/SimpleFlaskCloudAWS)"; fi
